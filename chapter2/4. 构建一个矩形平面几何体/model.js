adc7f94d00c4bfbfd744315e8814b9622c09b55c
cliff <clifflee1022@gmail.com>
2023-08-06 16:09:31 +0800

    feat: 4. 构建一个矩形平面几何体

---
+++ chapter2/4. 构建一个矩形平面几何体/model.js
@@ -0,0 +1,32 @@
+import * as THREE from 'three'
+
+// 创建一个空的几何体顶点对象
+const geometry = new THREE.BufferGeometry()
+
+//类型化数组创建顶点数据
+const vertices = new Float32Array([
+  0, 0, 0, //顶点1坐标
+  80, 0, 0, //顶点2坐标
+  80, 80, 0, //顶点3坐标
+  0, 0, 0, //顶点4坐标
+  80, 80, 0, //顶点5坐标
+  0, 80, 0, //顶点6坐标
+])
+
+// 创建属性缓冲区对象
+//3个为一组，表示一个顶点的xyz坐标
+const attribute = new THREE.BufferAttribute(vertices, 3)
+
+// 设置几何体attributes属性的位置属性
+geometry.attributes.position = attribute;
+
+const material = new THREE.MeshBasicMaterial({
+  color: 0x00ffff, // 材质颜色
+  // side: THREE.FrontSide, // 默认只有正面可见
+  side: THREE.DoubleSide, // 默认只有正面可见
+  // side: THREE.BackSide, // 默认只有正面可见
+})
+
+const mesh = new THREE.Mesh(geometry, material)
+
+export default mesh
